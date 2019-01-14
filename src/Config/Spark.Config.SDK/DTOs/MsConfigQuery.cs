340356460b2675910cb24ed53b807eac8738466d
王文兵 <22379099@qq.com>
2019-01-14 16:39:43 +0800

    添加健康检查

--- src/Config/Spark.Config.SDK/DTOs/MsConfigQuery.cs
+++ src/Config/Spark.Config.SDK/DTOs/MsConfigQuery.cs
@@ -9,7 +9,6 @@ namespace Spark.Config.SDK.DTOs
         public string App { get; set; }
 
         public string Key { get; set; }
-        public string Env { get; set; }
 
         public DateTime? UpdateTime { get; set; }
     }
